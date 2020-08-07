369ad9df3729dbfc543b916210b84c6bee454f98
happysoni0525 <ssongvely_@naver.com>
2020-08-08 01:52:31 +0900

    송사부수정했다 git status!

--- main/models.py
+++ main/models.py
@@ -21,6 +21,7 @@ class Subjects(models.Model):
     hospital = models.ForeignKey(Hospital , on_delete=models.CASCADE)
     subjects_name = models.CharField(max_length=20)
     specialist_cnt = models.IntegerField()
+    image = models.ImageField(blank=True)
     def __str__(self):
         return f'병원명 : {self.hospital.hospital_name} / 진료과목 : {self.subjects_name} / 전문의 수 : {self.specialist_cnt}'
     
